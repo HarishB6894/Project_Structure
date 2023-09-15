3d80336de52fdf5053f0cb3543e8022c4957be48
HarishB6894 <harishgokulnath@gmail.com>
2023-09-15 20:46:32 +0530

    new code

---
+++ src/test/java/org/base/main.java
@@ -0,0 +1,14 @@
+package org.base;
+
+import org.testng.annotations.Test;
+
+public class main {
+	
+	@Test
+	
+	private void test() {
+		System.out.println("Test 3");
+		System.out.println("Test 4");
+	}
+
+}
