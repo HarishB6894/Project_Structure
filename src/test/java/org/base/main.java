c5e961966b8f56e597013f1f8daa592c6b09b8a2
HarishB6894 <harishgokulnath@gmail.com>
2023-09-15 20:43:30 +0530

    Gokul

--- src/test/java/org/base/main.java
+++ src/test/java/org/base/main.java
@@ -1,7 +1,14 @@
 package org.base;
 
+import org.testng.annotations.Test;
+
 public class main {
 	
+	@Test
 	
+	private void test() {
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+	}
 
 }
